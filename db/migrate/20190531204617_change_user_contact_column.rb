be08aab0f844bfa2a4e9e5c85192b3fbaae5896b
logic101 <frknerdm@mail.com>
2019-06-01 07:18:46 +0300

    db

--- db/migrate/20190531204617_change_user_contact_column.rb
+++ db/migrate/20190531204617_change_user_contact_column.rb
@@ -1,6 +1,5 @@
 class ChangeUserContactColumn < ActiveRecord::Migration[5.2]
-  def change
-		remove_column :user_contacts, :contact_owner_id
-		add_reference :user_contacts, :companies
+  def change	
+		add_reference :user_contacts, :company
   end
 end
